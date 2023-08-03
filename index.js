4bec7e782f804f9877315e54ee51236e47fe3321
yuri herber <isabelleherberdarosa@gmail.com>
2023-08-03 09:27:09 -0300

    Front-END

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const { initializeApp } = require('firebase/app');
 const Auth = require('firebase/auth');
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyBeX-2DPk6F_ab-stDp7sN2FyxO5vuhngA",
+  authDomain: "petcare-connect.firebaseapp.com",
+  projectId: "petcare-connect",
+  storageBucket: "petcare-connect.appspot.com",
+  messagingSenderId: "737625730700",
+  appId: "1:737625730700:web:257c17a87c72241a6555a9"
 };
 const fireApp = initializeApp(firebaseConfig);
 const auth = Auth.getAuth();
